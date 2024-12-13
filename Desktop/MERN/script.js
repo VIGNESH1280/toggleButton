f4321d7fc2785c7a7b520103819fc5560bda6bb9
C.Vignesh <162482102+VIGNESH1280@users.noreply.github.com>
2024-12-13 22:48:49 +0530

    Delete Desktop/MERN directory

--- Desktop/MERN/script.js
+++
@@ -1,8 +0,0 @@
-
-const URL="https://jsonplaceholder.typicode.com/todos/3"
-const vignesh= async()=>{
-    const response = await fetch(URL)
-    const data = await response.json()
-    console.log(data);
-}
-vignesh()
